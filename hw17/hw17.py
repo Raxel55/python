c1f658b69d84f8d43b5aa2421103f23ec2935b7f
Alexey <leha.ust@mail.ru>
2020-02-12 14:01:51 +0300

    Fixed hw17

--- hw17/hw17.py
+++ hw17/hw17.py
@@ -10,8 +10,8 @@ mydict = Image.open(image)._getexif()
 
 gps = mydict.get(34853)
 if len(gps) != 0:
-    latitude = gps[2][0][0]/gps[2][0][1] + gps[2][1][0]/gps[2][1][1]/60
-    longitude = gps[4][0][0]/gps[4][0][1] + gps[4][1][0]/gps[4][1][1]/60
+    latitude = gps[2][0][0]/gps[2][0][1] + gps[2][1][0]/gps[2][1][1]/60 + gps[2][2][0]/gps[2][2][1]/3600   
+    longitude = gps[4][0][0]/gps[4][0][1] + gps[4][1][0]/gps[4][1][1]/60 + gps[4][2][0]/gps[4][2][1]/3600
     print(hw16.run((latitude, longitude)))
 else:
     print('Фотография не содержит геоданных')
